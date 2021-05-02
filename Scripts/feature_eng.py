a16a5fb4611b72dca2cc510e4daef9198cbc2688
Stankevix <Stankevix@users.noreply.github.com>
2021-05-01 21:26:16 -0300

    Add files via upload

--- Scripts/feature_eng.py
+++ Scripts/feature_eng.py
@@ -63,7 +63,7 @@ WHERE
 
 '''
 
-SQL3 = '''
+amazonia_sql = '''
 WITH meteor AS(
 	SELECT
 		CAST("Data" AS date) AS m_data,
@@ -88,7 +88,152 @@ FROM queimadas_brasil_reservas AS a,
 WHERE 
 	CAST(a.datahora AS date) = b.m_data 
 	AND trim(a.estado) = trim(b.m_estado)
+    AND a.bioma = 'Amazonia'
 '''
+
+
+caatinga_sql = '''
+WITH meteor AS(
+	SELECT
+		CAST("Data" AS date) AS m_data,
+		"uf" AS m_estado,
+		AVG("PRECIPITAÇÃO_TOTAL,_HORÁRIO_(mm)") AS avg_prep_total,
+		AVG("PRESSAO_ATMOSFERICA_AO_NIVEL_DA_ESTACAO,_HORARIA_(mB)") AS avg_pressao_atm,
+		AVG("PRESSÃO_ATMOSFERICA_MAX.NA_HORA_ANT._(AUT)_(mB)") AS avg_press_atm_max,
+		AVG("PRESSÃO_ATMOSFERICA_MIN._NA_HORA_ANT._(AUT)_(mB)") AS avg_press_atm_min,
+		AVG("RADIACAO_GLOBAL_(Kj/m²)") AS avg_rad_global,
+		AVG("TEMPERATURA_DO_AR___BULBO_SECO,_HORARIA_(°C)") AS avg_temp_ar,
+		AVG("UMIDADE_RELATIVA_DO_AR,_HORARIA_(%)") AS avg_umd_ar,
+		AVG("VENTO,_VELOCIDADE_HORARIA_(m/s)") AS avg_vento_velo
+	FROM metereologia
+	GROUP BY "Data", "uf"
+	ORDER BY "Data")
+
+SELECT
+	a.*,
+	b.*
+FROM queimadas_brasil_reservas AS a, 
+	meteor AS b
+WHERE 
+	CAST(a.datahora AS date) = b.m_data 
+	AND trim(a.estado) = trim(b.m_estado)
+    AND a.bioma = 'Caatinga'
+'''
+
+cerrado_sql = '''
+WITH meteor AS(
+	SELECT
+		CAST("Data" AS date) AS m_data,
+		"uf" AS m_estado,
+		AVG("PRECIPITAÇÃO_TOTAL,_HORÁRIO_(mm)") AS avg_prep_total,
+		AVG("PRESSAO_ATMOSFERICA_AO_NIVEL_DA_ESTACAO,_HORARIA_(mB)") AS avg_pressao_atm,
+		AVG("PRESSÃO_ATMOSFERICA_MAX.NA_HORA_ANT._(AUT)_(mB)") AS avg_press_atm_max,
+		AVG("PRESSÃO_ATMOSFERICA_MIN._NA_HORA_ANT._(AUT)_(mB)") AS avg_press_atm_min,
+		AVG("RADIACAO_GLOBAL_(Kj/m²)") AS avg_rad_global,
+		AVG("TEMPERATURA_DO_AR___BULBO_SECO,_HORARIA_(°C)") AS avg_temp_ar,
+		AVG("UMIDADE_RELATIVA_DO_AR,_HORARIA_(%)") AS avg_umd_ar,
+		AVG("VENTO,_VELOCIDADE_HORARIA_(m/s)") AS avg_vento_velo
+	FROM metereologia
+	GROUP BY "Data", "uf"
+	ORDER BY "Data")
+
+SELECT
+	a.*,
+	b.*
+FROM queimadas_brasil_reservas AS a, 
+	meteor AS b
+WHERE 
+	CAST(a.datahora AS date) = b.m_data 
+	AND trim(a.estado) = trim(b.m_estado)
+    AND a.bioma = 'Cerrado'
+'''
+
+m_atlantica_sql = '''
+WITH meteor AS(
+	SELECT
+		CAST("Data" AS date) AS m_data,
+		"uf" AS m_estado,
+		AVG("PRECIPITAÇÃO_TOTAL,_HORÁRIO_(mm)") AS avg_prep_total,
+		AVG("PRESSAO_ATMOSFERICA_AO_NIVEL_DA_ESTACAO,_HORARIA_(mB)") AS avg_pressao_atm,
+		AVG("PRESSÃO_ATMOSFERICA_MAX.NA_HORA_ANT._(AUT)_(mB)") AS avg_press_atm_max,
+		AVG("PRESSÃO_ATMOSFERICA_MIN._NA_HORA_ANT._(AUT)_(mB)") AS avg_press_atm_min,
+		AVG("RADIACAO_GLOBAL_(Kj/m²)") AS avg_rad_global,
+		AVG("TEMPERATURA_DO_AR___BULBO_SECO,_HORARIA_(°C)") AS avg_temp_ar,
+		AVG("UMIDADE_RELATIVA_DO_AR,_HORARIA_(%)") AS avg_umd_ar,
+		AVG("VENTO,_VELOCIDADE_HORARIA_(m/s)") AS avg_vento_velo
+	FROM metereologia
+	GROUP BY "Data", "uf"
+	ORDER BY "Data")
+
+SELECT
+	a.*,
+	b.*
+FROM queimadas_brasil_reservas AS a, 
+	meteor AS b
+WHERE 
+	CAST(a.datahora AS date) = b.m_data 
+	AND trim(a.estado) = trim(b.m_estado)
+    AND a.bioma = 'Mata Atlantica'
+'''
+
+
+pampa_sql = '''
+WITH meteor AS(
+	SELECT
+		CAST("Data" AS date) AS m_data,
+		"uf" AS m_estado,
+		AVG("PRECIPITAÇÃO_TOTAL,_HORÁRIO_(mm)") AS avg_prep_total,
+		AVG("PRESSAO_ATMOSFERICA_AO_NIVEL_DA_ESTACAO,_HORARIA_(mB)") AS avg_pressao_atm,
+		AVG("PRESSÃO_ATMOSFERICA_MAX.NA_HORA_ANT._(AUT)_(mB)") AS avg_press_atm_max,
+		AVG("PRESSÃO_ATMOSFERICA_MIN._NA_HORA_ANT._(AUT)_(mB)") AS avg_press_atm_min,
+		AVG("RADIACAO_GLOBAL_(Kj/m²)") AS avg_rad_global,
+		AVG("TEMPERATURA_DO_AR___BULBO_SECO,_HORARIA_(°C)") AS avg_temp_ar,
+		AVG("UMIDADE_RELATIVA_DO_AR,_HORARIA_(%)") AS avg_umd_ar,
+		AVG("VENTO,_VELOCIDADE_HORARIA_(m/s)") AS avg_vento_velo
+	FROM metereologia
+	GROUP BY "Data", "uf"
+	ORDER BY "Data")
+
+SELECT
+	a.*,
+	b.*
+FROM queimadas_brasil_reservas AS a, 
+	meteor AS b
+WHERE 
+	CAST(a.datahora AS date) = b.m_data 
+	AND trim(a.estado) = trim(b.m_estado)
+    AND a.bioma = 'Pampa'
+'''
+
+
+pantanal_sql = '''
+WITH meteor AS(
+	SELECT
+		CAST("Data" AS date) AS m_data,
+		"uf" AS m_estado,
+		AVG("PRECIPITAÇÃO_TOTAL,_HORÁRIO_(mm)") AS avg_prep_total,
+		AVG("PRESSAO_ATMOSFERICA_AO_NIVEL_DA_ESTACAO,_HORARIA_(mB)") AS avg_pressao_atm,
+		AVG("PRESSÃO_ATMOSFERICA_MAX.NA_HORA_ANT._(AUT)_(mB)") AS avg_press_atm_max,
+		AVG("PRESSÃO_ATMOSFERICA_MIN._NA_HORA_ANT._(AUT)_(mB)") AS avg_press_atm_min,
+		AVG("RADIACAO_GLOBAL_(Kj/m²)") AS avg_rad_global,
+		AVG("TEMPERATURA_DO_AR___BULBO_SECO,_HORARIA_(°C)") AS avg_temp_ar,
+		AVG("UMIDADE_RELATIVA_DO_AR,_HORARIA_(%)") AS avg_umd_ar,
+		AVG("VENTO,_VELOCIDADE_HORARIA_(m/s)") AS avg_vento_velo
+	FROM metereologia
+	GROUP BY "Data", "uf"
+	ORDER BY "Data")
+
+SELECT
+	a.*,
+	b.*
+FROM queimadas_brasil_reservas AS a, 
+	meteor AS b
+WHERE 
+	CAST(a.datahora AS date) = b.m_data 
+	AND trim(a.estado) = trim(b.m_estado)
+    AND a.bioma = 'Pantanal'
+'''
+
 
 # In[1]: Funcões
 
@@ -127,7 +272,7 @@ def create_var(df):
     df['minuto'] = pd.to_datetime(df['datahora']).dt.minute
     df['mes'] = pd.to_datetime(df['data']).dt.month
     df['quadrimestre'] = pd.to_datetime(df['data']).dt.quarter
-    df['dia_da_Semana'] = pd.to_datetime(df['data']).dt.dayofweek
+    df['dia_da_semana'] = pd.to_datetime(df['data']).dt.dayofweek
     
     df['diasemchuv'] = df['diasemchuv'].apply(lambda x: None if x <= 0 else x)
     df['riscofogo'] = df['riscofogo'].apply(lambda x: None if x <= 0 else x)
@@ -140,10 +285,11 @@ def create_var(df):
     return df
 
 def select_vars(df):
-    col_names = ['frp','estado','m_estado','municipio','bioma','riscofogo',
-            'diasemchuv','superficie','avg_prep_total',
-            'avg_pressao_atm','avg_press_atm_max','avg_press_atm_min',
-            'avg_rad_global','avg_temp_ar','avg_umd_ar','avg_vento_velo','hora']
+    col_names = ['frp','estado','m_estado','municipio',
+                 'bioma','riscofogo','diasemchuv','superficie',
+                 'avg_prep_total','avg_pressao_atm','avg_rad_global','avg_temp_ar',
+                 'avg_umd_ar','avg_vento_velo','hora','minuto',
+                 'mes','dia_da_semana', 'quadrimestre','flg_q_r']
     
     return df[col_names]
 
@@ -189,7 +335,6 @@ def lineplot_queimadas(df):
     plt.suptitle("Queimadas frp")
     plt.show()
 
-    
     return
 
 def export_data(df):
@@ -204,28 +349,61 @@ pwd = input("Informe a senha do database: ")
 conn = pgc.db_conn('localhost','spatial','postgres',pwd)
 conn.postgres()
 
-queimadas_raw = conn.get_dataframe(SQL3)
+amazonia_raw = conn.get_dataframe(amazonia_sql)
+caatinga__raw = conn.get_dataframe(caatinga_sql)
+cerrado_raw = conn.get_dataframe(cerrado_sql)
+m_atlantica_raw = conn.get_dataframe(m_atlantica_sql)
+pampa_raw = conn.get_dataframe(pampa_sql)
+pantanal_raw = conn.get_dataframe(pantanal_sql)
+
 
 # In[3]: Analise de dados
 #queimadas = get_encod(queimadas_raw)
-queimadas = select_vars(create_var(queimadas_raw))
-queimadas_corr = queimadas.corr()
+amazonia = select_vars(create_var(amazonia_raw))
+amazonia_corr = amazonia.corr()
 
-na_df = queimadas.isnull().sum(axis = 0)
+caatinga = select_vars(create_var(caatinga__raw))
+caatinga_corr = caatinga.corr()
+
+cerrado = select_vars(create_var(cerrado_raw))
+cerrado_corr = cerrado.corr()
+
+m_atlantica = select_vars(create_var(m_atlantica_raw))
+m_atlantica_corr = m_atlantica.corr()
+
+
+pampa = select_vars(create_var(pampa_raw))
+pampa_corr = pampa.corr()
+
+
+pantanal = select_vars(create_var(pantanal_raw))
+pantanal_corr = pantanal.corr()
+
+
+
+#na_df = queimadas.isnull().sum(axis = 0)
 
 # In[3]: Analise de dados
 
-queimadas_corr = queimadas.corr()
-queimadas_describe = queimadas.describe()
+#queimadas_corr = queimadas.corr()
+#queimadas_describe = queimadas.describe()
 
-na_df = queimadas.isnull().sum(axis = 0)
+
+
+#na_df = queimadas.isnull().sum(axis = 0)
 
 # In[4]: Analise de dados
 #lineplot_queimadas(queimadas)
-barplot_queimadas(queimadas)
+#barplot_queimadas(queimadas)
 
 # In[5]: Analise de dados
-export_data(queimadas)
+#export_data(queimadas)
+
+
+pantanal.to_csv('pantanal.csv',index=False)
+
+amazonia.to_csv('amazonia.csv',index=False)
+
 
 
    
